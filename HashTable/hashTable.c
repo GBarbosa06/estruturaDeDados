3594cc5c45a55e00e8b876c8e33c53f8789b97a5
gbarbosa06 <andradegui2006@gmail.com>
2026-06-09 22:42:46 -0300

    HashTable simples

---
+++ HashTable/hashTable.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 31
+
+void inicializarTabela(int t[]) {
+	for(int i = 0; i < TAM; i++){
+		t[i] = 0;
+	}
+}
+
+int funcaoHash(int chave) {
+	return chave % TAM;
+}
+
+void inserir(int t[], int valor){
+	int id = funcaoHash(valor);
+	while(t[id] != 0){
+		id = funcaoHash(id+1); 
+		//aqui define o id entre 0 e TAM(31), procurando posições vazias
+	}
+	t[id] = valor;
+}
+
+int busca(int t[], int chave){
+	int id = funcaoHash(chave);
+	while(t[id] != 0){
+		if(t[id] == chave){
+			printf("\tIndice %d\n", id);
+			return t[id];
+		}
+		else{
+			id = funcaoHash(id+1);
+		}
+	}
+	return 0;
+}
+
+void imprimir(int t[]){
+	for(int i = 0; i < TAM; i++){
+		printf("%d = %d\n", i, t[i]);
+	}
+}
+
+int main() {
+	int tabela[TAM], opcao, valor, retorno;
+	inicializarTabela(tabela);
+	
+	do{
+		printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Buscar\n\t3 - Imprimir\n");
+		scanf("%d", &opcao);
+
+		switch(opcao){
+			case 1:
+				printf("\tQual valor deseja inserir? ");
+				scanf("%d", &valor);
+				inserir(tabela, valor);
+				break;
+			case 2:
+				printf("\tQual valor deseja buscar? ");
+				scanf("%d", &valor);
+				retorno = busca(tabela, valor);
+				if(retorno != 0){
+					printf("\tValor encontrado: %d\n", retorno);
+				}
+				else{
+					printf("\tValor não encontrado!\n");
+				}
+				break;
+			case 3:
+				imprimir(tabela);
+				break;
+			default:
+				printf("Opcao invalida!\n");
+		}
+
+	}while(opcao != 0);
+
+
+	return 0;
+}
